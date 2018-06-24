f7934cdc3484e300b6af2a47ee85c8936abe2d98
Ahmed Elzoughby <gs_elzoughby@yahoo.com>
2018-06-24 14:29:52 +0200

    added some comments

--- src/startup_stm32h743xx.c
+++ src/startup_stm32h743xx.c
@@ -1,10 +1,30 @@
-/*============================================================================
+/*==============================================================================
  Name        : startup_stm32h743xx.c
- Author      : Ahmed Elzoughby
  Version     : 1.0.0
- Copyright   : Copyright (c) 2018 Ahmed Elzoughby
  Description : An independent startup code for STM32H7 MCUs written in C
-=============================================================================*/
+--------------------------------------------------------------------------------
+
+ The MIT License (MIT)
+ Copyright (c) 2018 Ahmed Elzoughby
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+===============================================================================*/
 
 
 
