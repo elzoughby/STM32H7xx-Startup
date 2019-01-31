fda8006123932e1b5a3c7a42bd258ef075acc6da
Ahmed Elzoughby <gs_elzoughby@yahoo.com>
2019-01-31 14:57:40 +0200

    replacing tabs with spaces for readability on github

--- src/startup_stm32h743xx.c
+++ src/startup_stm32h743xx.c
@@ -34,71 +34,71 @@
 
 /*===== define the Reset Clock Control registers bank =====*/
 typedef struct {
-	volatile uint32_t CR;
-	volatile uint32_t ICSCR;
-	volatile uint32_t CRRCR;
-	uint32_t RESERVED0;
-	volatile uint32_t CFGR;
-	uint32_t RESERVED1;
-	volatile uint32_t D1CFGR;
-	volatile uint32_t D2CFGR;
-	volatile uint32_t D3CFGR;
-	uint32_t RESERVED2;
-	volatile uint32_t PLLCKSELR;
-	volatile uint32_t PLLCFGR;
-	volatile uint32_t PLL1DIVR;
-	volatile uint32_t PLL1FRACR;
-	volatile uint32_t PLL2DIVR;
-	volatile uint32_t PLL2FRACR;
-	volatile uint32_t PLL3DIVR;
-	volatile uint32_t PLL3FRACR;
-	uint32_t RESERVED3;
-	volatile uint32_t D1CCIPR;
-	volatile uint32_t D2CCIP1R;
-	volatile uint32_t D2CCIP2R;
-	volatile uint32_t D3CCIPR;
-	uint32_t RESERVED4;
-	volatile uint32_t CIER;
-	volatile uint32_t CIFR;
-	volatile uint32_t CICR;
-	uint32_t RESERVED5;
-	volatile uint32_t BDCR;
-	volatile uint32_t CSR;
-	uint32_t RESERVED6;
-	volatile uint32_t AHB3RSTR;
-	volatile uint32_t AHB1RSTR;
-	volatile uint32_t AHB2RSTR;
-	volatile uint32_t AHB4RSTR;
-	volatile uint32_t APB3RSTR;
-	volatile uint32_t APB1LRSTR;
-	volatile uint32_t APB1HRSTR;
-	volatile uint32_t APB2RSTR;
-	volatile uint32_t APB4RSTR;
-	volatile uint32_t GCR;
-	uint32_t RESERVED7;
-	volatile uint32_t D3AMR;
-	uint32_t RESERVED8[9];
-	volatile uint32_t RSR;
-	volatile uint32_t AHB3ENR;
-	volatile uint32_t AHB1ENR;
-	volatile uint32_t AHB2ENR;
-	volatile uint32_t AHB4ENR;
-	volatile uint32_t APB3ENR;
-	volatile uint32_t APB1LENR;
-	volatile uint32_t APB1HENR;
-	volatile uint32_t APB2ENR;
-	volatile uint32_t APB4ENR;
-	uint32_t RESERVED9;
-	volatile uint32_t AHB3LPENR;
-	volatile uint32_t AHB1LPENR;
-	volatile uint32_t AHB2LPENR;
-	volatile uint32_t AHB4LPENR;
-	volatile uint32_t APB3LPENR;
-	volatile uint32_t APB1LLPENR;
-	volatile uint32_t APB1HLPENR;
-	volatile uint32_t APB2LPENR;
-	volatile uint32_t APB4LPENR;
-	uint32_t RESERVED10[4];
+    volatile uint32_t CR;
+    volatile uint32_t ICSCR;
+    volatile uint32_t CRRCR;
+    uint32_t RESERVED0;
+    volatile uint32_t CFGR;
+    uint32_t RESERVED1;
+    volatile uint32_t D1CFGR;
+    volatile uint32_t D2CFGR;
+    volatile uint32_t D3CFGR;
+    uint32_t RESERVED2;
+    volatile uint32_t PLLCKSELR;
+    volatile uint32_t PLLCFGR;
+    volatile uint32_t PLL1DIVR;
+    volatile uint32_t PLL1FRACR;
+    volatile uint32_t PLL2DIVR;
+    volatile uint32_t PLL2FRACR;
+    volatile uint32_t PLL3DIVR;
+    volatile uint32_t PLL3FRACR;
+    uint32_t RESERVED3;
+    volatile uint32_t D1CCIPR;
+    volatile uint32_t D2CCIP1R;
+    volatile uint32_t D2CCIP2R;
+    volatile uint32_t D3CCIPR;
+    uint32_t RESERVED4;
+    volatile uint32_t CIER;
+    volatile uint32_t CIFR;
+    volatile uint32_t CICR;
+    uint32_t RESERVED5;
+    volatile uint32_t BDCR;
+    volatile uint32_t CSR;
+    uint32_t RESERVED6;
+    volatile uint32_t AHB3RSTR;
+    volatile uint32_t AHB1RSTR;
+    volatile uint32_t AHB2RSTR;
+    volatile uint32_t AHB4RSTR;
+    volatile uint32_t APB3RSTR;
+    volatile uint32_t APB1LRSTR;
+    volatile uint32_t APB1HRSTR;
+    volatile uint32_t APB2RSTR;
+    volatile uint32_t APB4RSTR;
+    volatile uint32_t GCR;
+    uint32_t RESERVED7;
+    volatile uint32_t D3AMR;
+    uint32_t RESERVED8[9];
+    volatile uint32_t RSR;
+    volatile uint32_t AHB3ENR;
+    volatile uint32_t AHB1ENR;
+    volatile uint32_t AHB2ENR;
+    volatile uint32_t AHB4ENR;
+    volatile uint32_t APB3ENR;
+    volatile uint32_t APB1LENR;
+    volatile uint32_t APB1HENR;
+    volatile uint32_t APB2ENR;
+    volatile uint32_t APB4ENR;
+    uint32_t RESERVED9;
+    volatile uint32_t AHB3LPENR;
+    volatile uint32_t AHB1LPENR;
+    volatile uint32_t AHB2LPENR;
+    volatile uint32_t AHB4LPENR;
+    volatile uint32_t APB3LPENR;
+    volatile uint32_t APB1LLPENR;
+    volatile uint32_t APB1HLPENR;
+    volatile uint32_t APB2LPENR;
+    volatile uint32_t APB4LPENR;
+    uint32_t RESERVED10[4];
 } rccStruct;
 
 
@@ -283,174 +283,174 @@ extern uint32_t _ebss;
 /* The Interrupt Vector Table */
 __attribute__ ((section(".isr_vector")))
 void (* const g_pfnVectors[])(void) = {
-		((void (*)(void)) (&_estack)),
-		Reset_Handler,
-		NMI_Handler,
-		HardFault_Handler,
-		MemManage_Handler,
-		BusFault_Handler,
-		UsageFault_Handler,
-		0,
-		0,
-		0,
-		0,
-		SVC_Handler,
-		DebugMon_Handler,
-		0,
-		PendSV_Handler,
-		SysTick_Handler,
+        ((void (*)(void)) (&_estack)),
+        Reset_Handler,
+        NMI_Handler,
+        HardFault_Handler,
+        MemManage_Handler,
+        BusFault_Handler,
+        UsageFault_Handler,
+        0,
+        0,
+        0,
+        0,
+        SVC_Handler,
+        DebugMon_Handler,
+        0,
+        PendSV_Handler,
+        SysTick_Handler,
 
-		/* External Interrupts */
-		WWDG_IRQHandler,
-		PVD_AVD_IRQHandler,
-		TAMP_STAMP_IRQHandler,
-		RTC_WKUP_IRQHandler,
-		FLASH_IRQHandler,
-		RCC_IRQHandler,
-		EXTI0_IRQHandler,
-		EXTI1_IRQHandler,
-		EXTI2_IRQHandler,
-		EXTI3_IRQHandler,
-		EXTI4_IRQHandler,
-		DMA1_Stream0_IRQHandler,
-		DMA1_Stream1_IRQHandler,
-		DMA1_Stream2_IRQHandler,
-		DMA1_Stream3_IRQHandler,
-		DMA1_Stream4_IRQHandler,
-		DMA1_Stream5_IRQHandler,
-		DMA1_Stream6_IRQHandler,
-		ADC_IRQHandler,
-		FDCAN1_IT0_IRQHandler,
-		FDCAN2_IT0_IRQHandler,
-		FDCAN1_IT1_IRQHandler,
-		FDCAN2_IT1_IRQHandler,
-		EXTI9_5_IRQHandler,
-		TIM1_BRK_IRQHandler,
-		TIM1_UP_IRQHandler,
-		TIM1_TRG_COM_IRQHandler,
-		TIM1_CC_IRQHandler,
-		TIM2_IRQHandler,
-		TIM3_IRQHandler,
-		TIM4_IRQHandler,
-		I2C1_EV_IRQHandler,
-		I2C1_ER_IRQHandler,
-		I2C2_EV_IRQHandler,
-		I2C2_ER_IRQHandler,
-		SPI1_IRQHandler,
-		SPI2_IRQHandler,
-		USART1_IRQHandler,
-		USART2_IRQHandler,
-		USART3_IRQHandler,
-		EXTI15_10_IRQHandler,
-		RTC_Alarm_IRQHandler,
-		0,
-		TIM8_BRK_TIM12_IRQHandler,
-		TIM8_UP_TIM13_IRQHandler,
-		TIM8_TRG_COM_TIM14_IRQHandler,
-		TIM8_CC_IRQHandler,
-		DMA1_Stream7_IRQHandler,
-		FMC_IRQHandler,
-		SDMMC1_IRQHandler,
-		TIM5_IRQHandler,
-		SPI3_IRQHandler,
-		UART4_IRQHandler,
-		UART5_IRQHandler,
-		TIM6_DAC_IRQHandler,
-		TIM7_IRQHandler,
-		DMA2_Stream0_IRQHandler,
-		DMA2_Stream1_IRQHandler,
-		DMA2_Stream2_IRQHandler,
-		DMA2_Stream3_IRQHandler,
-		DMA2_Stream4_IRQHandler,
-		ETH_IRQHandler,
-		ETH_WKUP_IRQHandler,
-		FDCAN_CAL_IRQHandler,
-		0,
-		0,
-		0,
-		0,
-		DMA2_Stream5_IRQHandler,
-		DMA2_Stream6_IRQHandler,
-		DMA2_Stream7_IRQHandler,
-		USART6_IRQHandler,
-		I2C3_EV_IRQHandler,
-		I2C3_ER_IRQHandler,
-		OTG_HS_EP1_OUT_IRQHandler,
-		OTG_HS_EP1_IN_IRQHandler,
-		OTG_HS_WKUP_IRQHandler,
-		OTG_HS_IRQHandler,
-		DCMI_IRQHandler,
-		0,
-		RNG_IRQHandler,
-		FPU_IRQHandler,
-		UART7_IRQHandler,
-		UART8_IRQHandler,
-		SPI4_IRQHandler,
-		SPI5_IRQHandler,
-		SPI6_IRQHandler,
-		SAI1_IRQHandler,
-		LTDC_IRQHandler,
-		LTDC_ER_IRQHandler,
-		DMA2D_IRQHandler,
-		SAI2_IRQHandler,
-		QUADSPI_IRQHandler,
-		LPTIM1_IRQHandler,
-		CEC_IRQHandler,
-		I2C4_EV_IRQHandler,
-		I2C4_ER_IRQHandler,
-		SPDIF_RX_IRQHandler,
-		OTG_FS_EP1_OUT_IRQHandler,
-		OTG_FS_EP1_IN_IRQHandler,
-		OTG_FS_WKUP_IRQHandler,
-		OTG_FS_IRQHandler,
-		DMAMUX1_OVR_IRQHandler,
-		HRTIM1_Master_IRQHandler,
-		HRTIM1_TIMA_IRQHandler,
-		HRTIM1_TIMB_IRQHandler,
-		HRTIM1_TIMC_IRQHandler,
-		HRTIM1_TIMD_IRQHandler,
-		HRTIM1_TIME_IRQHandler,
-		HRTIM1_FLT_IRQHandler,
-		DFSDM1_FLT0_IRQHandler,
-		DFSDM1_FLT1_IRQHandler,
-		DFSDM1_FLT2_IRQHandler,
-		DFSDM1_FLT3_IRQHandler,
-		SAI3_IRQHandler,
-		SWPMI1_IRQHandler,
-		TIM15_IRQHandler,
-		TIM16_IRQHandler,
-		TIM17_IRQHandler,
-		MDIOS_WKUP_IRQHandler,
-		MDIOS_IRQHandler,
-		JPEG_IRQHandler,
-		MDMA_IRQHandler,
-		0,
-		SDMMC2_IRQHandler,
-		HSEM1_IRQHandler,
-		0,
-		ADC3_IRQHandler,
-		DMAMUX2_OVR_IRQHandler,
-		BDMA_Channel0_IRQHandler,
-		BDMA_Channel1_IRQHandler,
-		BDMA_Channel2_IRQHandler,
-		BDMA_Channel3_IRQHandler,
-		BDMA_Channel4_IRQHandler,
-		BDMA_Channel5_IRQHandler,
-		BDMA_Channel6_IRQHandler,
-		BDMA_Channel7_IRQHandler,
-		COMP1_IRQHandler,
-		LPTIM2_IRQHandler,
-		LPTIM3_IRQHandler,
-		LPTIM4_IRQHandler,
-		LPTIM5_IRQHandler,
-		LPUART1_IRQHandler,
-		0,
-		CRS_IRQHandler,
-		0,
-		SAI4_IRQHandler,
-		0,
-		0,
-		WAKEUP_PIN_IRQHandler
+        /* External Interrupts */
+        WWDG_IRQHandler,
+        PVD_AVD_IRQHandler,
+        TAMP_STAMP_IRQHandler,
+        RTC_WKUP_IRQHandler,
+        FLASH_IRQHandler,
+        RCC_IRQHandler,
+        EXTI0_IRQHandler,
+        EXTI1_IRQHandler,
+        EXTI2_IRQHandler,
+        EXTI3_IRQHandler,
+        EXTI4_IRQHandler,
+        DMA1_Stream0_IRQHandler,
+        DMA1_Stream1_IRQHandler,
+        DMA1_Stream2_IRQHandler,
+        DMA1_Stream3_IRQHandler,
+        DMA1_Stream4_IRQHandler,
+        DMA1_Stream5_IRQHandler,
+        DMA1_Stream6_IRQHandler,
+        ADC_IRQHandler,
+        FDCAN1_IT0_IRQHandler,
+        FDCAN2_IT0_IRQHandler,
+        FDCAN1_IT1_IRQHandler,
+        FDCAN2_IT1_IRQHandler,
+        EXTI9_5_IRQHandler,
+        TIM1_BRK_IRQHandler,
+        TIM1_UP_IRQHandler,
+        TIM1_TRG_COM_IRQHandler,
+        TIM1_CC_IRQHandler,
+        TIM2_IRQHandler,
+        TIM3_IRQHandler,
+        TIM4_IRQHandler,
+        I2C1_EV_IRQHandler,
+        I2C1_ER_IRQHandler,
+        I2C2_EV_IRQHandler,
+        I2C2_ER_IRQHandler,
+        SPI1_IRQHandler,
+        SPI2_IRQHandler,
+        USART1_IRQHandler,
+        USART2_IRQHandler,
+        USART3_IRQHandler,
+        EXTI15_10_IRQHandler,
+        RTC_Alarm_IRQHandler,
+        0,
+        TIM8_BRK_TIM12_IRQHandler,
+        TIM8_UP_TIM13_IRQHandler,
+        TIM8_TRG_COM_TIM14_IRQHandler,
+        TIM8_CC_IRQHandler,
+        DMA1_Stream7_IRQHandler,
+        FMC_IRQHandler,
+        SDMMC1_IRQHandler,
+        TIM5_IRQHandler,
+        SPI3_IRQHandler,
+        UART4_IRQHandler,
+        UART5_IRQHandler,
+        TIM6_DAC_IRQHandler,
+        TIM7_IRQHandler,
+        DMA2_Stream0_IRQHandler,
+        DMA2_Stream1_IRQHandler,
+        DMA2_Stream2_IRQHandler,
+        DMA2_Stream3_IRQHandler,
+        DMA2_Stream4_IRQHandler,
+        ETH_IRQHandler,
+        ETH_WKUP_IRQHandler,
+        FDCAN_CAL_IRQHandler,
+        0,
+        0,
+        0,
+        0,
+        DMA2_Stream5_IRQHandler,
+        DMA2_Stream6_IRQHandler,
+        DMA2_Stream7_IRQHandler,
+        USART6_IRQHandler,
+        I2C3_EV_IRQHandler,
+        I2C3_ER_IRQHandler,
+        OTG_HS_EP1_OUT_IRQHandler,
+        OTG_HS_EP1_IN_IRQHandler,
+        OTG_HS_WKUP_IRQHandler,
+        OTG_HS_IRQHandler,
+        DCMI_IRQHandler,
+        0,
+        RNG_IRQHandler,
+        FPU_IRQHandler,
+        UART7_IRQHandler,
+        UART8_IRQHandler,
+        SPI4_IRQHandler,
+        SPI5_IRQHandler,
+        SPI6_IRQHandler,
+        SAI1_IRQHandler,
+        LTDC_IRQHandler,
+        LTDC_ER_IRQHandler,
+        DMA2D_IRQHandler,
+        SAI2_IRQHandler,
+        QUADSPI_IRQHandler,
+        LPTIM1_IRQHandler,
+        CEC_IRQHandler,
+        I2C4_EV_IRQHandler,
+        I2C4_ER_IRQHandler,
+        SPDIF_RX_IRQHandler,
+        OTG_FS_EP1_OUT_IRQHandler,
+        OTG_FS_EP1_IN_IRQHandler,
+        OTG_FS_WKUP_IRQHandler,
+        OTG_FS_IRQHandler,
+        DMAMUX1_OVR_IRQHandler,
+        HRTIM1_Master_IRQHandler,
+        HRTIM1_TIMA_IRQHandler,
+        HRTIM1_TIMB_IRQHandler,
+        HRTIM1_TIMC_IRQHandler,
+        HRTIM1_TIMD_IRQHandler,
+        HRTIM1_TIME_IRQHandler,
+        HRTIM1_FLT_IRQHandler,
+        DFSDM1_FLT0_IRQHandler,
+        DFSDM1_FLT1_IRQHandler,
+        DFSDM1_FLT2_IRQHandler,
+        DFSDM1_FLT3_IRQHandler,
+        SAI3_IRQHandler,
+        SWPMI1_IRQHandler,
+        TIM15_IRQHandler,
+        TIM16_IRQHandler,
+        TIM17_IRQHandler,
+        MDIOS_WKUP_IRQHandler,
+        MDIOS_IRQHandler,
+        JPEG_IRQHandler,
+        MDMA_IRQHandler,
+        0,
+        SDMMC2_IRQHandler,
+        HSEM1_IRQHandler,
+        0,
+        ADC3_IRQHandler,
+        DMAMUX2_OVR_IRQHandler,
+        BDMA_Channel0_IRQHandler,
+        BDMA_Channel1_IRQHandler,
+        BDMA_Channel2_IRQHandler,
+        BDMA_Channel3_IRQHandler,
+        BDMA_Channel4_IRQHandler,
+        BDMA_Channel5_IRQHandler,
+        BDMA_Channel6_IRQHandler,
+        BDMA_Channel7_IRQHandler,
+        COMP1_IRQHandler,
+        LPTIM2_IRQHandler,
+        LPTIM3_IRQHandler,
+        LPTIM4_IRQHandler,
+        LPTIM5_IRQHandler,
+        LPUART1_IRQHandler,
+        0,
+        CRS_IRQHandler,
+        0,
+        SAI4_IRQHandler,
+        0,
+        0,
+        WAKEUP_PIN_IRQHandler
 };
 
 
@@ -465,10 +465,10 @@ void Default_Handler() {
 
 void Reset_Handler() {
 
-	/* set stack pointer */
+    /* set stack pointer */
     __asm("    ldr    sp, =_estack");
 
-	/* Call the clock system initialization function */
+    /* Call the clock system initialization function */
     SystemInit();
 
     /* Copy the data segment initializers from flash to SRAM */
@@ -505,7 +505,7 @@ void NMI_Handler() {
 
 void HardFault_Handler() {
 
-	/* Go into an infinite loop */
+    /* Go into an infinite loop */
     while(1) {
 
     }
@@ -513,7 +513,7 @@ void HardFault_Handler() {
 
 void MemManage_Handler() {
 
-	/* Go into an infinite loop */
+    /* Go into an infinite loop */
     while(1) {
 
     }
@@ -521,7 +521,7 @@ void MemManage_Handler() {
 
 void BusFault_Handler() {
 
-	/* Go into an infinite loop */
+    /* Go into an infinite loop */
     while(1) {
 
     }
@@ -529,7 +529,7 @@ void BusFault_Handler() {
 
 void UsageFault_Handler() {
 
-	/* Go into an infinite loop */
+    /* Go into an infinite loop */
     while(1) {
 
     }
@@ -537,7 +537,7 @@ void UsageFault_Handler() {
 
 void SVC_Handler() {
 
-	/* Go into an infinite loop */
+    /* Go into an infinite loop */
     while(1) {
 
     }
@@ -545,7 +545,7 @@ void SVC_Handler() {
 
 void DebugMon_Handler() {
 
-	/* Go into an infinite loop */
+    /* Go into an infinite loop */
     while(1) {
 
     }
@@ -553,7 +553,7 @@ void DebugMon_Handler() {
 
 void PendSV_Handler() {
 
-	/* Go into an infinite loop */
+    /* Go into an infinite loop */
     while(1) {
 
     }
@@ -561,7 +561,7 @@ void PendSV_Handler() {
 
 void SysTick_Handler() {
 
-	/* Go into an infinite loop */
+    /* Go into an infinite loop */
     while(1) {
 
     }
@@ -570,45 +570,45 @@ void SysTick_Handler() {
 
 static void SystemInit(void) {
 
-	/* Enable the floating-point unit. Any configuration of the
-	floating-point unit must be done here prior to it being enabled */
-	HWREG(0xE000ED88) = ((HWREG(0xE000ED88) & ~0x00F00000) | 0x00F00000);
+    /* Enable the floating-point unit. Any configuration of the
+    floating-point unit must be done here prior to it being enabled */
+    HWREG(0xE000ED88) = ((HWREG(0xE000ED88) & ~0x00F00000) | 0x00F00000);
 
-	/*------- Reset the RCC clock configuration to the default reset state -------*/
-	/* Set HSION bit */
-	RCC->CR |= 0x00000001;
-	/* Reset CFGR register */
-	RCC->CFGR = 0x00000000;
-	/* Reset HSEON, CSSON , CSION,RC48ON, CSIKERON PLL1ON, PLL2ON and PLL3ON bits */
-	RCC->CR &= (uint32_t)0xEAF6ED7F;
-	/* Reset D1CFGR register */
-	RCC->D1CFGR = 0x00000000;
-	/* Reset D2CFGR register */
-	RCC->D2CFGR = 0x00000000;
-	/* Reset D3CFGR register */
-	RCC->D3CFGR = 0x00000000;
-	/* Reset PLLCKSELR register */
-	RCC->PLLCKSELR = 0x00000000;
-	/* Reset PLLCFGR register */
-	RCC->PLLCFGR = 0x00000000;
-	/* Reset PLL1DIVR register */
-	RCC->PLL1DIVR = 0x00000000;
-	/* Reset PLL1FRACR register */
-	RCC->PLL1FRACR = 0x00000000;
-	/* Reset PLL2DIVR register */
-	RCC->PLL2DIVR = 0x00000000;
-	/* Reset PLL2FRACR register */
-	RCC->PLL2FRACR = 0x00000000;
-	/* Reset PLL3DIVR register */
-	RCC->PLL3DIVR = 0x00000000;
-	/* Reset PLL3FRACR register */
-	RCC->PLL3FRACR = 0x00000000;
-	/* Reset HSEBYP bit */
-	RCC->CR &= (uint32_t)0xFFFBFFFF;
-	/* Disable all interrupts */
-	RCC->CIER = 0x00000000;
+    /*------- Reset the RCC clock configuration to the default reset state -------*/
+    /* Set HSION bit */
+    RCC->CR |= 0x00000001;
+    /* Reset CFGR register */
+    RCC->CFGR = 0x00000000;
+    /* Reset HSEON, CSSON , CSION,RC48ON, CSIKERON PLL1ON, PLL2ON and PLL3ON bits */
+    RCC->CR &= (uint32_t)0xEAF6ED7F;
+    /* Reset D1CFGR register */
+    RCC->D1CFGR = 0x00000000;
+    /* Reset D2CFGR register */
+    RCC->D2CFGR = 0x00000000;
+    /* Reset D3CFGR register */
+    RCC->D3CFGR = 0x00000000;
+    /* Reset PLLCKSELR register */
+    RCC->PLLCKSELR = 0x00000000;
+    /* Reset PLLCFGR register */
+    RCC->PLLCFGR = 0x00000000;
+    /* Reset PLL1DIVR register */
+    RCC->PLL1DIVR = 0x00000000;
+    /* Reset PLL1FRACR register */
+    RCC->PLL1FRACR = 0x00000000;
+    /* Reset PLL2DIVR register */
+    RCC->PLL2DIVR = 0x00000000;
+    /* Reset PLL2FRACR register */
+    RCC->PLL2FRACR = 0x00000000;
+    /* Reset PLL3DIVR register */
+    RCC->PLL3DIVR = 0x00000000;
+    /* Reset PLL3FRACR register */
+    RCC->PLL3FRACR = 0x00000000;
+    /* Reset HSEBYP bit */
+    RCC->CR &= (uint32_t)0xFFFBFFFF;
+    /* Disable all interrupts */
+    RCC->CIER = 0x00000000;
 
-	/* Change the switch matrix read issuing capability to 1 for the AXI SRAM target (Target 7) */
-	HWREG(0x51008108) = 0x000000001;
+    /* Change the switch matrix read issuing capability to 1 for the AXI SRAM target (Target 7) */
+    HWREG(0x51008108) = 0x000000001;
 }
 
